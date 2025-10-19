663c7afc50c92d4c3efdc262c56f2e25db4c9c58
varsha <varshabharathi.642@gmail.com>
2025-10-19 10:14:32 +0530

    Created the controller

---
+++ app/Http/Controllers/CustomerController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Customer;
+use App\Exports\CustomerExport;
+use Maatwebsite\Excel\Facades\Excel;
+use Maatwebsite\Excel\Facades\Excel as Download;
+
+class CustomerController extends Controller
+{
+    public function index(){
+    $customer = Customer::all();
+    return view('frontend.customer.index', compact('customer'));
+    }
+
+    public function export(){
+        return Excel::download(new CustomerExport, 'Customer.xlsx');
+    }
+}                                                                           // if ($request->ajax()) {
+                                                                            //             $data = User::select('*');
+                                                                            //             return DataTables::of($data)->make(true);
+                                                                            //         }
+                                                                            //         return view('users.index');
+
